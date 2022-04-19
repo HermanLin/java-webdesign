9bbb64f0c5847693011cea040a5ad1fb695985f5
HermanLin <hl3213@nyu.edu>
2022-04-19 18:12:05 -0400

    edited comment at top of files

--- hw4/Server.java
+++ hw4/Server.java
@@ -1,23 +1,8 @@
 // Herman Lin
 /******************************************************************* 
 Server.java
-    This is the Server of a simple Client/Server chat room service.
 
-In order to run the full program suite: 
-1) First, compile the necessary files:
-    
-    $ javac Server.java
-    $ javac Client.java
-    $ javac ChatterBox.java
-
-2) Then, run the Server:
-    
-    $ java Server
-
-3) Finally, open any number of clients using ChatterBox:
-
-    $ java ChatterBox
-
+This is the Server of a simple Client/Server chat room service.
 *******************************************************************/
 
 import java.io.*;
